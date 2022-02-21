dc6bbc84c691480b1320e286652072777e0bb2c7
Rigoberto martinez <donojaci@gmail.com>
2022-02-21 13:52:24 -0600

    agregamos un archivo css para darle estilos a la pagina y queriamos aplicar cupones pero ubo un error que todavia no encuentro

--- descuentos/descuentos.js
+++ descuentos/descuentos.js
@@ -11,6 +11,11 @@
 //     PorcentajePrecioDescuento,
 //     PrecioConDescuento
 // })
+var Cupones =[
+    "Platzi",
+    "ASUS",
+    "Asking Alexandria"
+]
 
 function Descuentos(PrecioOriginal, Descuento){
     PorcentajePrecioDescuento = 100 - Descuento;
@@ -29,10 +34,53 @@ function OnClickButtonPriceDiscount(){
     const InputPrice = document.getElementById("InputPrice");
     const PriceValue = InputPrice.value;
     const InputDiscount = document.getElementById("InputDiscount");
-    const DiscountValue = InputDiscount.value
+    const DiscountValue = InputDiscount.value;
+    // const InputCupon = document.getElementById("InputCoupons");
+    // const CuponValue = InputCupon.value;
 
+    // let descuento;
+
+    // switch(CuponValue){
+    //     case Cupones[0]:
+    //         descuento = 15;
+    //     break;
+    //     case Cupones[1]:
+    //         descuento = 30;
+    //     break;
+    //     case Cupones[2]:
+    //         descuento = 25;
+    //     break 
+    // }
     const PrecioConDescuento = Descuentos(PriceValue, DiscountValue)
 
     const ResultP = document.getElementById("ResultPrice")
     ResultP.innerText = "El precio con descuento es $ "+ PrecioConDescuento
 }
+
+// function onClickButtonPriceDiscount() {
+//     const inputPrice = document.getElementById("InputPrice");
+//     const priceValue = inputPrice.value;
+    
+//     const inputCoupon = document.getElementById("InputCoupons");
+//     const couponValue = inputCoupon.value;
+
+//     let descuento;
+
+//     switch(couponValue) {
+//       case Cupones[0]: // "JuanDC_es_Batman"
+//         descuento = 15;
+//     break;
+//       case Cupones[1]: // "pero_no_le_digas_a_nadie"
+//         descuento = 30;
+//     break;
+//       case Cupones[2]: // "es_un_secreto"
+//         descuento = 25;
+//     break;
+//     }
+
+
+//     const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+//     const resultP = document.getElementById("ResultP");
+//     resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+// }
