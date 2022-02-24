9b5d8d3e8a900197f7a544d9b7c29967fdedc5b3
Rigoberto martinez <donojaci@gmail.com>
2022-02-24 12:34:36 -0600

    aplicando la moda a los salarios en colombia

--- analisisSalarial/analisis.js
+++ analisisSalarial/analisis.js
@@ -1,10 +1,41 @@
+function calcularPromedio(lista){
+    const sumaLista = lista.reduce(
+        function (ValorAcumulcado = 0, nuevoElemento){
+            return ValorAcumulcado + nuevoElemento;
+        }
+    );
+    const PromedioLista = sumaLista/lista.length;
+    return PromedioLista;
+}
+
+
 const salariosCol = colombia.map(
     function(persona){
         return persona.salary;
     }
 )
 
-salariosCol.sort(function(a,b){
+const salarioColSort = salariosCol.sort(function(a,b){
     return a - b;
-})
+}
+);
 
+function CalcularMdiana(list){
+    const midlista = parseInt(list.length/2)
+
+    function esPar(Elemento){
+        return (Elemento % 2 === 0);
+    }
+
+    if(esPar(list.length)){
+        const num1 = list[midlista]
+        const num2 = list[midlista - 1]
+
+        const mediana = calcularPromedio([num1, num2])
+        return mediana 
+    }else{
+        return list[midlista]
+    }
+} 
+
+console.log(CalcularMdiana(salarioColSort))
